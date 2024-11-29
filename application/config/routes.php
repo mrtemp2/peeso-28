8119baae88b58f53831a3ddf1c8c0e64a315a295
Anis Selmani <selmanii300@gmail.com>
2024-11-29 17:38:32 +0100

    "updated database"

--- application/config/routes.php
+++ application/config/routes.php
@@ -68,6 +68,3 @@ $route['google_callback'] = 'Users/google_callback';
 $route['common/setEtabId'] = 'common/setEtabId';
 $route['users/fetchAdministrateurEtab'] = 'users/fetchAdministrateurEtab';
 
-$route['users/fetchAdministrateurEtab1'] = 'users/fetchAdministrateurEtab1';
-
-
